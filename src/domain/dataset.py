f4081ea5de0f70bfe29078ce2b5d8caf81143d3b
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-01-23 12:37:11 -0500

    Better dev mode

--- src/domain/dataset.py
+++ src/domain/dataset.py
@@ -68,42 +68,13 @@ class CnnDailyMailDataset(SummarizationDataset):
         }
 
     def _load_all(self, dev):
-        val_set = self._load_split('val')
-        test_set = self._load_split('test')
+        train_set = self._load_split('train', dev)
+        val_set = self._load_split('val', dev)
+        test_set = self._load_split('test', dev)
 
-        if dev:
-            dev_set = self._check_dev_set(test_set)
-            return dev_set, val_set, test_set
-        else:
-            train_set = self._load_split('train')
-            return train_set, val_set, test_set
+        return train_set, val_set, test_set
 
-    def _check_dev_set(self, test_set):
-        reading_path = os.path.join(self.path, 'finished_files', 'dev')
-
-        if not os.path.isdir(reading_path):
-            logging.info(
-                'No dev set built yet. Building it from the test set now.')
-
-            out_file = reading_path + '.tar'
-
-            with tarfile.open(out_file, 'w') as writer:
-                for idx in range(100):
-                    article = test_set[idx]
-                    js_example = {}
-                    js_example['id'] = article.id
-                    js_example['article'] = article.content
-                    js_example['abstract'] = article.abstract
-                    js_serialized = json.dumps(js_example, indent=4).encode()
-                    save_file = io.BytesIO(js_serialized)
-                    tar_info = tarfile.TarInfo('{}/{}.json'.format(
-                        os.path.basename(out_file).replace('.tar', ''), idx))
-                    tar_info.size = len(js_serialized)
-                    writer.addfile(tar_info, save_file)
-
-        return self._load_split('dev')
-
-    def _load_split(self, split):
+    def _load_split(self, split, dev):
         finished_path = os.path.join(self.path, 'finished_files')
         reading_path = os.path.join(finished_path, split)
 
@@ -115,8 +86,12 @@ class CnnDailyMailDataset(SummarizationDataset):
                 tar.extractall(finished_path)
 
         all_articles = []
+        all_files = os.listdir(reading_path)
 
-        for fname in datetime_tqdm(os.listdir(reading_path),
+        if dev:
+            all_files = all_files[:100]
+
+        for fname in datetime_tqdm(all_files,
                                    desc=f'Reading {split} files...'):
             with open(os.path.join(reading_path, fname), 'r') as data:
                 all_articles.append(Example.fromJSON(data.read(), self.fields))
