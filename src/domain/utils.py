f1744b9556bb1de7776f11c561d1933395c16bb8
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-01-27 10:16:23 -0500

    Add random seeding

--- src/domain/utils.py
+++ src/domain/utils.py
@@ -1,6 +1,9 @@
 import logging
 import sys
 import time
+import numpy as np
+import torch
+import random
 
 from datetime import datetime
 from tqdm import tqdm
@@ -27,3 +30,11 @@ def get_formatted_desc(desc=''):
 
 def datetime_tqdm(iterable, *, desc='', **kwargs):
     return tqdm(iterable, desc=get_formatted_desc(desc), **kwargs)
+
+
+def set_random_seed(seed):
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
