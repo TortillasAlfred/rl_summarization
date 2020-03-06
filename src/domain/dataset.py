63067da059590686d0ea919ad36abade6c56ad1b
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-03-05 20:18:24 -0500

    Load articles in memory

--- src/domain/dataset.py
+++ src/domain/dataset.py
@@ -21,7 +21,7 @@ class SummarizationDataset(Dataset):
         self.subsets = OrderedDict(
             [(key, Dataset(subset, fields, filter_pred)) for key, subset in subsets]
         )
-        # self._build_vocabs(vectors, vectors_cache)
+        self._build_vocabs(vectors, vectors_cache)
 
     def _build_vocabs(self):
         raise NotImplementedError()
@@ -111,7 +111,7 @@ class CnnDailyMailDataset(SummarizationDataset):
         for fname in datetime_tqdm(all_files, desc=f"Reading {split} files..."):
             fpath = os.path.join(reading_path, fname)
             with open(fpath, "r") as data:
-                # all_articles.append(Example.fromJSON(data.read(), self.fields))
+                all_articles.append(Example.fromJSON(data.read(), self.fields))
                 all_paths.append(fpath)
 
         return all_articles, all_paths
@@ -155,7 +155,9 @@ if __name__ == "__main__":
 
     logging.info("Begin")
 
-    dataset = CnnDailyMailDataset("./data/cnn_dailymail", "glove.6B.100d", dev=True, sets=["train"])
+    dataset = CnnDailyMailDataset(
+        "./data/cnn_dailymail", "glove.6B.100d", dev=True, sets=["train"]
+    )
 
     train_set = dataset.get_splits()["train"]
 
