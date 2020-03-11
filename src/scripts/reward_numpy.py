3bfbd8628ad1ac072cf73eda3a671f57688a4151
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-03-10 22:28:39 -0400

    direct rouge npy saving from articles

--- src/scripts/reward_numpy.py
+++ src/scripts/reward_numpy.py
@@ -6,39 +6,71 @@ import os
 import argparse
 import json
 import numpy as np
+import pickle
 
 from joblib import Parallel, delayed
 from collections import OrderedDict
+from itertools import permutations
 from math import ceil
 
 
 @delayed
-def process_sample(fname, saving_dir, read_dir):
-    with open(os.path.join(read_dir, fname), "rb") as f:
+def process_sample(fpath, saving_dir, bad_files_path):
+    with open(fpath, "rb") as f:
         data = json.load(f)
 
-    n_sents = ceil(len(data) ** (1 / 3)) + 1
-    assert n_sents * (n_sents - 1) * (n_sents - 2) == len(data)
-    matrix_data = np.zeros((n_sents, n_sents, n_sents, 3), dtype=np.float32)
-    for key, rouge in data.items():
-        idx = np.asarray(key.split('-'), dtype=int)
-        matrix_data[tuple(idx)] = np.asarray(rouge)
+    if "rouge" in data:
+        rouge_scores = data["rouge"]
 
-    f_id = fname.split(".")[0]
-    np.save(os.path.join(saving_dir, f_id), matrix_data)
+        del data["rouge"]
+
+        n_sents = ceil(len(rouge_scores) ** (1 / 3)) + 1
+        assert n_sents * (n_sents - 1) * (n_sents - 2) == len(data)
+        matrix_data = np.zeros((n_sents, n_sents, n_sents, 3), dtype=np.float32)
+        for key, rouge in data.items():
+            idx = np.asarray(key.split("-"), dtype=int)
+
+            for i in permutations(idx):
+                matrix_data[tuple(idx)] = np.asarray(rouge)
+
+        np.save(os.path.join(saving_dir, data["id"]), matrix_data)
+
+        with open(fpath, "w", encoding="utf8") as f:
+            json.dump(data, f)
+    else:
+        with open(bad_files_path, "rb") as f:
+            bad_files = pickle.load(f)
+
+        bad_files.append(fpath)
+
+        with open(bad_files_path, "wb") as f:
+            pickle.dump(bad_files, f)
 
 
 def main(options):
     configure_logging()
 
+    bad_files_path = os.path.join(options.saving_dir, "bad_train_files.pck")
+
+    with open(bad_files_path, "wb") as f:
+        pickle.dump([], f)
+
+    dataset = CnnDailyMailDataset(
+        options.data_path,
+        "glove.6B.100d",
+        vectors_cache=options.vectors_cache,
+        sets=[options.dataset],
+        dev=options.dev,
+    )
+
     saving_dir = os.path.join(options.target_dir, options.dataset)
     os.makedirs(saving_dir, exist_ok=True)
 
-    read_dir = os.path.join(options.source_dir, options.dataset)
-
     Parallel(n_jobs=-1)(
-        process_sample(fname, saving_dir, read_dir)
-        for fname in datetime_tqdm(os.listdir(read_dir), desc="Saving rouge scores")
+        process_sample(fname, saving_dir, bad_files_path)
+        for fname in datetime_tqdm(
+            dataset.fpaths[options.dataset], desc="Saving rouge scores"
+        )
     )
 
     logging.info("Done")
@@ -59,5 +91,6 @@ if __name__ == "__main__":
     argument_parser.add_argument(
         "--source_dir", type=str, default="./data/cnn_dailymail/rouge/"
     )
+    argument_parser.add_argument("--dev", action="store_true")
     options = argument_parser.parse_args()
     main(options)
