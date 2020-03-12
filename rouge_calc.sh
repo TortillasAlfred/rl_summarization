29c70380c6b08df84657adb9d1efe7e1f6f18c9b
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-03-11 23:30:41 -0400

    Launcher ready

--- rouge_calc.sh
+++ rouge_calc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --array=0-958                                        
-#SBATCH --account=def-lulam50                                 # Account with resources
+#SBATCH --array=0-719                                        
+#SBATCH --account=rrg-corbeilj-ac                             # Account with resources
 #SBATCH --cpus-per-task=8                                     # Number of CPUs
-#SBATCH --mem=5G                                              # memory (per node)
-#SBATCH --time=0-12:00                                        # time (DD-HH:MM)
+#SBATCH --mem=10G                                              # memory (per node)
+#SBATCH --time=0-03:00                                        # time (DD-HH:MM)
 #SBATCH --mail-user=mathieu.godbout.3@ulaval.ca               # Where to email
 #SBATCH --mail-type=FAIL                                      # Email when a job fails
 #SBATCH --output=/scratch/magod/rouge_calc/%A_%a.out          # Default write output on scratch, to jobID_arrayID.out file
@@ -18,7 +18,7 @@ SECONDS=0
 # The $@ transfers all args passed to this bash file to the Python script
 # i.e. a call to 'sbatch $sbatch_args this_launcher.sh --arg1=0 --arg2=True'
 # will call 'python my_script.py --arg1=0 --arg2=True'
-python -um src.scripts.rouge $SLURM_ARRAY_TASK_ID --data_path=/scratch/magod/summarization_datasets/cnn_dailymail/data --vectors_cache=/scratch/magod/embeddings/ 
+python -um src.scripts.rouge $SLURM_ARRAY_TASK_ID --data_path=/scratch/magod/summarization_datasets/cnn_dailymail/data --vectors_cache=/scratch/magod/embeddings/ --target_dir=/scratch/magod/summarization_datasets/cnn_dailymail/data/rouge_npy/ --dataset=train
 
 # Utility to show job duration in output file
 diff=$SECONDS
