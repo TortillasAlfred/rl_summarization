7cff8d19d4c36306ec96432d682e9e856e9ec63f
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-04-09 03:55:08 -0400

    24 hour job

--- launch_training.sh
+++ launch_training.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=4                                                      # Number of CPUs
 #SBATCH --gres=gpu:1                                                           # Number of GPUs
 #SBATCH --mem=35G                                                              # memory (per node)
-#SBATCH --time=0-12:00                                                         # time (DD-HH:MM)
+#SBATCH --time=0-24:00                                                         # time (DD-HH:MM)
 #SBATCH --mail-user=mathieu.godbout.3@ulaval.ca                                # Where to email
 #SBATCH --mail-type=FAIL                                                       # Email when a job fails
 #SBATCH --output=/project/def-lulam50/magod/rl_summ/slurm_outputs/%A_%a.out    # Default write output on scratch, to jobID_arrayID.out file
