1661e3518147b5a60fc9d34b94900be247720030
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-04-09 01:24:43 -0400

    train run

--- rouge_calc.sh
+++ rouge_calc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --array=0-1                                        
+#SBATCH --array=0-50                                        
 #SBATCH --account=def-corbeilj                                # Account with resources
-#SBATCH --cpus-per-task=8                                     # Number of CPUs
-#SBATCH --mem=10G                                             # memory (per node)
+#SBATCH --cpus-per-task=12                                     # Number of CPUs
+#SBATCH --mem=15G                                             # memory (per node)
 #SBATCH --time=0-02:00                                        # time (DD-HH:MM)
 #SBATCH --mail-user=mathieu.godbout.3@ulaval.ca               # Where to email
 #SBATCH --mail-type=FAIL                                      # Email when a job fails
@@ -20,7 +20,7 @@ SECONDS=0
 # The $@ transfers all args passed to this bash file to the Python script
 # i.e. a call to 'sbatch $sbatch_args this_launcher.sh --arg1=0 --arg2=True'
 # will call 'python my_script.py --arg1=0 --arg2=True'
-python -um src.scripts.rouge $SLURM_ARRAY_TASK_ID --data_path=/scratch/magod/summarization_datasets/cnn_dailymail/data --vectors_cache=/scratch/magod/embeddings/ --target_dir=/scratch/magod/summarization_datasets/cnn_dailymail/data/rouge_npy/ --dataset=val
+python -um src.scripts.rouge $SLURM_ARRAY_TASK_ID --data_path=/scratch/magod/summarization_datasets/cnn_dailymail/data --vectors_cache=/scratch/magod/embeddings/ --target_dir=/scratch/magod/summarization_datasets/cnn_dailymail/data/rouge_npy/ --dataset=train
 
 # Utility to show job duration in output file
 diff=$SECONDS
