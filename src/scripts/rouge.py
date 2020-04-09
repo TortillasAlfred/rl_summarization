ecad1c72b05b0124b73ad4436a28f6cce8c4978a
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-04-09 01:12:53 -0400

    fix bad rouge files

--- src/scripts/rouge.py
+++ src/scripts/rouge.py
@@ -11,7 +11,7 @@ import pickle
 
 from itertools import combinations, permutations
 
-SLICE_SIZE = 400
+SLICE_SIZE = 600
 
 
 def main(options):
@@ -19,42 +19,39 @@ def main(options):
 
     reward = RougeReward(n_jobs=-1)
 
-    for dataset in ["test", "val", "train"]:
-        save_dir = os.path.join(options.target_dir, dataset)
+    save_dir = os.path.join(options.target_dir, options.dataset)
 
-        os.makedirs(save_dir, exist_ok=True)
+    os.makedirs(save_dir, exist_ok=True)
 
-        bad_files_dir = "/scratch/magod/summarization_datasets/bad_files/"
+    with open(f"bad_files_{options.dataset}.pck", "rb") as f:
+        iterable = pickle.load(f)[
+            options.run_index * SLICE_SIZE : (options.run_index + 1) * SLICE_SIZE
+        ]
 
-        with open(os.path.join(bad_files_dir, f"{dataset}.pck"), "rb") as f:
-            iterable = pickle.load(f)
+    for fpath in datetime_tqdm(iterable, desc="Calculating rouge scores"):
+        with open(fpath, "rb") as f:
+            article = json.load(f)
+        all_summ_idxs = list(combinations(range(len(article["article"])), 3))
+        all_summs = [[article["article"][i] for i in idxs] for idxs in all_summ_idxs]
+        all_refs = [article["abstract"] for _ in all_summs]
 
-        for fpath in datetime_tqdm(iterable, desc="Calculating rouge scores"):
-            with open(fpath, "rb") as f:
-                article = json.load(f)
-            all_summ_idxs = list(combinations(range(len(article["article"])), 3))
-            all_summs = [
-                [article["article"][i] for i in idxs] for idxs in all_summ_idxs
-            ]
-            all_refs = [article["abstract"] for _ in all_summs]
+        rouge_scores = reward(all_summs, all_refs, "cpu").squeeze().tolist()
+        n_sents = len(article["article"])
 
-            rouge_scores = reward(all_summs, all_refs, "cpu").squeeze().tolist()
-            n_sents = len(article["article"])
+        matrix_data = np.zeros((n_sents, n_sents, n_sents, 3), dtype=np.float32)
+        for idxs, rouge in zip(all_summ_idxs, rouge_scores):
 
-            matrix_data = np.zeros((n_sents, n_sents, n_sents, 3), dtype=np.float32)
-            for idxs, rouge in zip(all_summ_idxs, rouge_scores):
+            for i in permutations(idxs):
+                matrix_data[tuple(idxs)] = np.asarray(rouge)
 
-                for i in permutations(idxs):
-                    matrix_data[tuple(idxs)] = np.asarray(rouge)
-
-            fname = fpath.split("/")[-1].split(".")[0]
-            np.save(os.path.join(save_dir, fname), matrix_data)
+        np.save(os.path.join(save_dir, f'{article["id"]}.npy'), matrix_data)
 
     logging.info("Done")
 
 
 if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("run_index", type=int)
     argument_parser.add_argument(
         "--data_path", type=str, default="./data/cnn_dailymail"
     )
