2f8979dc66b6305a36c4a912233ce05c3afd20e1
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-04-09 01:17:16 -0400

    run for val

--- rouge_calc.sh
+++ rouge_calc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash                                        
-#SBATCH --account=def-lulam50                                 # Account with resources
+#SBATCH --account=def-corbeilj                                # Account with resources
 #SBATCH --cpus-per-task=8                                     # Number of CPUs
 #SBATCH --mem=10G                                             # memory (per node)
 #SBATCH --time=0-01:00                                        # time (DD-HH:MM)
@@ -19,7 +19,7 @@ SECONDS=0
 # The $@ transfers all args passed to this bash file to the Python script
 # i.e. a call to 'sbatch $sbatch_args this_launcher.sh --arg1=0 --arg2=True'
 # will call 'python my_script.py --arg1=0 --arg2=True'
-python -um src.scripts.rouge $SLURM_ARRAY_TASK_ID --data_path=/scratch/magod/summarization_datasets/cnn_dailymail/data --vectors_cache=/scratch/magod/embeddings/ --target_dir=/scratch/magod/summarization_datasets/cnn_dailymail/data/rouge_npy/ --dataset=test
+python -um src.scripts.rouge $SLURM_ARRAY_TASK_ID --data_path=/scratch/magod/summarization_datasets/cnn_dailymail/data --vectors_cache=/scratch/magod/embeddings/ --target_dir=/scratch/magod/summarization_datasets/cnn_dailymail/data/rouge_npy/ --dataset=val
 
 # Utility to show job duration in output file
 diff=$SECONDS
