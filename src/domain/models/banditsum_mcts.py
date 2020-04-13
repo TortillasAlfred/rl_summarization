54a1c6eda009ffb3d824d0f9a4c27d98e8380cfd
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-04-13 00:22:33 -0400

    return to cross-entropy loss

--- src/domain/models/banditsum_mcts.py
+++ src/domain/models/banditsum_mcts.py
@@ -137,8 +137,7 @@ class BanditSumMCTS(pl.LightningModule):
                 mcts_idxs, Categorical(probs=mcts_probs)
             )
 
-            loss = (mcts_probs.to(valid_sentences.device) * action_dist.probs) ** 2
-            loss = loss.sum()
+            loss = (mcts_probs.to(valid_sentences.device) * action_dist.logits).sum()
 
             return mcts_rewards, greedy_rewards, loss
         else:
