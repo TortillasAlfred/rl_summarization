2c27efcbad0c67007b2dc83e41abca0f24b10500
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-05-12 03:33:22 -0400

    test tube launcher for oful

--- src/scripts/test_tube_launcher.py
+++ src/scripts/test_tube_launcher.py
@@ -17,11 +17,11 @@ def optimize_on_cluster(hparams):
     )
 
     # configure cluster
-    cluster.per_experiment_nb_cpus = 10
+    cluster.per_experiment_nb_cpus = 16
     cluster.per_experiment_nb_gpus = 1
     cluster.per_experiment_nb_nodes = 1
     cluster.job_time = "24:00:00"
-    cluster.gpu_type = "v100l"
+    cluster.gpu_type = "p100"
     cluster.memory_mb_per_node = int(6e4)
     cluster.minutes_to_checkpoint_before_walltime = 1
 
@@ -32,15 +32,34 @@ def optimize_on_cluster(hparams):
     )
 
     cluster.optimize_parallel_cluster_gpu(
-        main, nb_trials=3, job_name="rl_summarization"
+        main, nb_trials=20, job_name="rl_summarization"
     )
 
 
 if __name__ == "__main__":
     base_configs = yaml.load(open("./configs/base.yaml"), Loader=yaml.FullLoader)
     argument_parser = HyperOptArgumentParser(strategy="random_search")
+
+    fine_tuned_items = {}
+
+    fine_tuned_items["--C"] = dict(
+        default=0.0, type=float, tunable=True, options=[0.0, 0.5, 1.0]
+    )
+    fine_tuned_items["--delta"] = dict(
+        default=0.0001, type=float, tunable=True, options=[0.01, 0.0001, 0.000001]
+    )
+    fine_tuned_items["--D_t_source"] = dict(
+        default="word-level",
+        type=str,
+        tunable=True,
+        options=["word-level", "sentence-level"],
+    )
+    fine_tuned_items["--n_mcts_samples"] = dict(
+        default=250, type=int, tunable=True, options=[50, 100, 250, 500]
+    )
+
     for config, value in base_configs.items():
-        if config not in ["c_puct"]:
+        if config not in fine_tuned_items:
             if type(value) is bool:
                 # Hack as per https://stackoverflow.com/a/46951029
                 argument_parser.add_argument(
@@ -52,11 +71,9 @@ if __name__ == "__main__":
                 argument_parser.add_argument(
                     "--{}".format(config), type=type(value), default=value
                 )
-    # let's enable optimizing over the number of layers in the network
 
-    argument_parser.opt_list(
-        "--c_puct", default=0.1, type=float, tunable=True, options=[0.05, 0.1, 0.25]
-    )
+    for name, kwargs in fine_tuned_items.items():
+        argument_parser.opt_list(name, **kwargs)
 
     hparams = argument_parser.parse_args()
     optimize_on_cluster(hparams)
