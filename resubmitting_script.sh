e36bfc438a3584341348bfb886c342d520a16f28
Mathieu Godbout <mathieu.godbout.3@ulaval.ca>
2020-08-26 20:22:31 -0400

    Good 2 go

---
+++ resubmitting_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --account=def-lulam50
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:03:00
+#SBATCH --job-name=resubmission
+#SBATCH --mem=1G
+
+sleep $1
+sbatch $2
+
+
+
+
+
+
