59c639f746055ee753c639964a791cdd46330c90
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-12-02 13:26:16 -0500

    p100 gpus

--- src/scripts/test_tube_launcher.py
+++ src/scripts/test_tube_launcher.py
@@ -21,7 +21,7 @@ def optimize_on_cluster(hparams):
     cluster.per_experiment_nb_gpus = 1
     cluster.per_experiment_nb_nodes = 1
     cluster.job_time = "1-00:00:00"
-    cluster.gpu_type = "t4"
+    cluster.gpu_type = "p100"
     cluster.memory_mb_per_node = int(1e5)
     cluster.minutes_to_checkpoint_before_walltime = 2
 
