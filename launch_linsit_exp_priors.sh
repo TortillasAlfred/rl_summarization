78457c089fbf42065ff710d9e1c6ca8f7c35b38e
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-12-13 01:38:21 -0500

    [WIP] linSIT exp priors

---
+++ launch_linsit_exp_priors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=def-adurand                                                  # Account with resources
+#SBATCH --gres=gpu:t4:4                                                        # Number of GPUs
+#SBATCH --cpus-per-task=16                                                     # Number of CPUs
+#SBATCH --mem=80G                                                              # memory (per node)
+#SBATCH --time=0-12:00                                                         # time (DD-HH:MM)
+#SBATCH --mail-user=mathieu.godbout.3@ulaval.ca                                # Where to email
+#SBATCH --mail-type=FAIL                                                       # Email when a job fails
+#SBATCH --output=/project/def-lulam50/magod/rl_summ/slurm_outputs/%A_%a.out    # Default write output on scratch, to jobID_arrayID.out file
+#SBATCH --signal=SIGUSR1@90                                                    # Killing signal 90 seconds before job end
+
+mkdir /project/def-lulam50/magod/rl_summ/slurm_outputs/
+
+source ~/venvs/default/bin/activate
+
+python -um src.scripts.linsit_exp
