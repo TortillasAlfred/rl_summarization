0fa02f779ddfc47073fe510c5cb7cacfd7d0ee43
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-12-13 17:36:58 -0500

    typo

--- src/domain/linsit.py
+++ src/domain/linsit.py
@@ -119,7 +119,7 @@ def linsit_exp_prior(
         action_vectors, priors, scores, n_samples, c_puct, device,
     )
 
-    priors - priors.detach().cpu().numpy()
+    priors = priors.detach().cpu().numpy()
     best_idxs = np.argpartition(priors, -3)[-3:]
     prior_max_score = scores[tuple(best_idxs)].mean()
     prior_max_proba = priors[best_idxs].sum()
