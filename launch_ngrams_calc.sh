2d326d8018f0e7c298aa9a72b2485837f3106f36
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-12-17 09:39:28 -0500

    Relaunch

--- launch_ngrams_calc.sh
+++ launch_ngrams_calc.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-#SBATCH --account=def-lulam50                                                  # Account with resources
-#SBATCH --cpus-per-task=32                                                     # Number of CPUs
-#SBATCH --mem=50G                                                              # memory (per node)
-#SBATCH --time=0-24:00                                                         # time (DD-HH:MM)
+#SBATCH --account=rrg-corbeilj-ac                                              # Account with resources
+#SBATCH --cpus-per-task=8                                                     # Number of CPUs
+#SBATCH --mem=30G                                                              # memory (per node)
+#SBATCH --time=0-06:00                                                         # time (DD-HH:MM)
 #SBATCH --mail-user=mathieu.godbout.3@ulaval.ca                                # Where to email
 #SBATCH --mail-type=FAIL                                                       # Email when a job fails
 #SBATCH --output=/project/def-lulam50/magod/rl_summ/slurm_outputs/%A.out       # Default write output on scratch, to jobID_arrayID.out file
 #SBATCH --signal=SIGUSR1@90                                                    # Killing signal 90 seconds before job end
 
 mkdir /project/def-lulam50/magod/rl_summ/slurm_outputs/
+
+mkdir $SLURM_TEMPDIR/finished_files/
+cp /scratch/magod/summarization_datasets/cnn_dailymail/data/finished_files/*.tar $SLURM_TEMPDIR/finished_files/
 
 source ~/venvs/default/bin/activate
 
