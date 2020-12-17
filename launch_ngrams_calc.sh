1a74402b9eb5808590a226bd890f01496d11c3ed
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-12-17 08:14:33 -0500

    ngrams_calc on Helios

--- launch_ngrams_calc.sh
+++ launch_ngrams_calc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#SBATCH --account=rrg-corbeilj-ac                                              # Account with resources
-#SBATCH --cpus-per-task=32                                                     # Number of CPUs
+#SBATCH --account=def-adurand                                                  # Account with resources
+#SBATCH --gres=gpu:k80:1                                                       # Number of GPUs
+#SBATCH --cpus-per-task=24                                                     # Number of CPUs
 #SBATCH --mem=50G                                                              # memory (per node)
 #SBATCH --time=0-24:00                                                         # time (DD-HH:MM)
 #SBATCH --mail-user=mathieu.godbout.3@ulaval.ca                                # Where to email
@@ -12,5 +13,6 @@
 mkdir /project/def-lulam50/magod/rl_summ/slurm_outputs/
 
 source ~/venvs/default/bin/activate
+cd ~/git/rl_summarization
 
 python -um src.scripts.ngrams_calc
