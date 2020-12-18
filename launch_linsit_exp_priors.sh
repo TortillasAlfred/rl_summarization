4b26013331e1e46ff2ccca0651716b3921a80776
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-12-18 00:59:43 -0500

    More fun slurm logging

--- launch_linsit_exp_priors.sh
+++ launch_linsit_exp_priors.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=0-12:00                                                         # time (DD-HH:MM)
 #SBATCH --mail-user=mathieu.godbout.3@ulaval.ca                                # Where to email
 #SBATCH --mail-type=FAIL                                                       # Email when a job fails
-#SBATCH --output=/project/def-lulam50/magod/rl_summ/slurm_outputs/%A_%a.out    # Default write output on scratch, to jobID_arrayID.out file
+#SBATCH --output=/project/def-lulam50/magod/rl_summ/slurm_outputs/%A.out       # Default write output on scratch, to jobID_arrayID.out file
 #SBATCH --signal=SIGUSR1@90                                                    # Killing signal 90 seconds before job end
 
 mkdir /project/def-lulam50/magod/rl_summ/slurm_outputs/
