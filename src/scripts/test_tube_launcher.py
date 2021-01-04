4f6ecfdcafb38a43bf17c0143d7e02636e753c15
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2021-01-03 22:13:07 -0500

    Better prio

--- src/scripts/test_tube_launcher.py
+++ src/scripts/test_tube_launcher.py
@@ -22,7 +22,7 @@ def optimize_on_cluster(hparams):
     cluster.per_experiment_nb_nodes = 1
     cluster.job_time = "3-00:00:00"
     cluster.gpu_type = "t4"
-    cluster.memory_mb_per_node = int(1e5)
+    cluster.memory_mb_per_node = 48000
     cluster.minutes_to_checkpoint_before_walltime = 2
 
     # any modules for code to run in env
