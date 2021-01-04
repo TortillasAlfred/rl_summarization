ef387e623441b6fd39b6283f18d34a2995502b64
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2021-01-04 12:19:04 -0500

    Ready for real launch

--- src/scripts/test_tube_launcher_sit_priors.py
+++ src/scripts/test_tube_launcher_sit_priors.py
@@ -17,10 +17,10 @@ def optimize_on_cluster(hparams):
     )
 
     # configure cluster
-    cluster.per_experiment_nb_cpus = 11
+    cluster.per_experiment_nb_cpus = 8
     cluster.per_experiment_nb_gpus = 1
     cluster.per_experiment_nb_nodes = 1
-    cluster.job_time = "3-00:00:00"
+    cluster.job_time = "1-00:00:00"
     cluster.gpu_type = "p100"
     cluster.memory_mb_per_node = 40000
     cluster.minutes_to_checkpoint_before_walltime = 2
