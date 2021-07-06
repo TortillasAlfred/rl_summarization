c7aa9eab36c542475fba7cd1787df8fa06b19a25
Jérémie Gauthier <jegauth@gra-login2.graham.sharcnet>
2021-07-06 08:49:45 -0400

    Fixed launch_bertcombisum_exp.sh

--- launch_bertcombisum_exp.sh
+++ launch_bertcombisum_exp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #SBATCH --account=rrg-corbeilj-ac                                              # Account with resources
 #SBATCH --cpus-per-task=4                                                      # Number of CPUs
 #SBATCH --gres=gpu:t4:1                                                        # Number of GPUs (per node)
@@ -9,12 +10,25 @@
 #SBATCH --output=/project/def-adurand/jegauth/rl_summ/output/%A.out            # Default write output on scratch, to jobID.out file
 #SBATCH --signal=SIGUSR1@90                                                    # Killing signal 90 seconds before job end
 
-cd /home/jegauth/main/rl_summarization
-./../scripts/unzip_dataset.sh
+echo "TRAIN RL SUMMARIZATION"
+echo "### IMPORTANT: Create /output, /results and /weights directories ###"
 
+mkdir /project/def-adurand/jegauth/rl_summ/output/
+
+# Load the full dataset in $SLURM_TMPDIR/
+cp /home/jegauth/main/data.zip $SLURM_TMPDIR/
+cd $SLURM_TMPDIR/
+unzip -qq data.zip
+
+# Change directory
+cd /home/jegauth/main/rl_summarization
+
+# Activate virtual environnement
 source /home/jegauth/main/DS_env/bin/activate
 
+# Load the necessary modules
 module load StdEnv/2020 gcc/9.3.0 arrow/2.0.0
 module load python/3.8.2 scipy-stack
 
+# Run the program
 /home/jegauth/main/DS_env/bin/python3 -m src.scripts.training --data_path $SLURM_TMPDIR/data --default_save_path /project/def-adurand/jegauth/rl_summ/results --weights_save_path /project/def-adurand/jegauth/rl_summ/weights
