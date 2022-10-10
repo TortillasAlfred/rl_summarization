21a89904e9839100105418706f65221371806303
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 00:46:03 +0000

    Adding tarfile member sanitization to extractall()

--- src/domain/rewards/rouge.py
+++ src/domain/rewards/rouge.py
@@ -68,7 +68,26 @@ class RougeRewardBuilder:
                 reading_path = self.base_path + ".tar"
             with tarfile.open(reading_path) as tar:
                 logging.info(f"Rewards not yet extracted to {self.base_path} folder. Doing it now.")
-                tar.extractall(base_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, base_path)
 
     def init_scorer(self, article_id, subset):
         assert subset in ["train", "test", "val"]
